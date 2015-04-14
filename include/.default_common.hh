cf3ba3b80710a8b491b4ed40322f507e7f0b4287
Matthias W. Smith <mwsmith2@uw.edu>
2015-04-14 14:11:52 -0700

    Added a macro that generates root branch strings and fixed the nmr_data vector container resize method.

--- include/.default_common.hh
+++ include/.default_common.hh
@@ -80,17 +80,39 @@ struct drs4 {
   UShort_t trace[DRS4_CH][DRS4_LN];
 };
 
+// Built from basic structs 
+struct event_data {
+  std::vector<sis_3350> sis_fast;
+  std::vector<sis_3302> sis_slow;
+  std::vector<caen_1785> caen_adc;
+  std::vector<caen_6742> caen_drs;
+  std::vector<drs4> drs;
+};
+
+// NMR specific stuff
 // A macro to define nmr structs since they are very similar.
 #define MAKE_NMR_STRUCT(name, num_ch, len_tr)\
 struct name {\
-  ULong64_t clock_sec[num_ch];\
-  ULong64_t clock_usec[num_ch];\
-  Double_t fid_time[num_ch];\
+  Double_t sys_clock[num_ch];\
+  Double_t gps_clock[num_ch];\
+  Double_t dev_clock[num_ch];\
   Double_t snr[num_ch];\
+  Double_t len[num_ch];\
   Double_t freq[num_ch];\
-  Double_t freq_err[num_ch];\
-  UShort_t  trace[num_ch][len_tr];\
+  Double_t ferr[num_ch];\
+  Double_t freq_zc[num_ch];\
+  Double_t ferr_zc[num_ch];\
+  UShort_t health[num_ch];
+  UShort_t method[num_ch];
+  UShort_t trace[num_ch][len_tr];\
 };\
+
+// Might as well define a root branch string for the struct.
+#define MAKE_NMR_STRING(name, num_ch, len_tr)\
+"sys_clock[num_ch]/D:gps_clock[num_ch]/D:dev_clock[num_ch]/D:\
+snr[num_ch]/D:len[num_ch]/D:freq[num_ch]/D:ferr[num_ch]\D:\
+freq_zc[num_ch]/D:ferr_zc[num_ch]/D:health[num_ch]\s:\
+method[num_ch]/s:trace[num_ch][len_tr]"
 
 MAKE_NMR_STRUCT(shim_platform, SHIM_PLATFORM_CH, NMR_FID_LN);
 MAKE_NMR_STRUCT(shim_platform_st, SHIM_PLATFORM_CH, SHORT_FID_LN);
@@ -102,32 +124,43 @@ MAKE_NMR_STRUCT(run_trolley_st, RUN_TROLLEY_CH, SHORT_FID_LN);
 MAKE_NMR_STRUCT(run_fixed, RUN_FIXED_CH, NMR_FID_LN);
 MAKE_NMR_STRUCT(run_fixed_st, RUN_FIXED_CH, SHORT_FID_LN);
 
-// flexible struct built from basic structs.
-// Built from basic structs 
-struct event_data {
-  std::vector<sis_3350> sis_fast;
-  std::vector<sis_3302> sis_slow;
-  std::vector<caen_1785> caen_adc;
-  std::vector<caen_6742> caen_drs;
-  std::vector<drs4> drs;
-};
+MAKE_NMR_STRING(shim_platform, SHIM_PLATFORM_CH, NMR_FID_LN);
+MAKE_NMR_STRING(shim_platform_st, SHIM_PLATFORM_CH, SHORT_FID_LN);
+MAKE_NMR_STRING(shim_fixed, SHIM_FIXED_CH, NMR_FID_LN);
+MAKE_NMR_STRING(shim_fixed_st, SHIM_FIXED_CH, SHORT_FID_LN);
 
+MAKE_NMR_STRING(run_trolley, RUN_TROLLEY_CH, NMR_FID_LN);
+MAKE_NMR_STRING(run_trolley_st, RUN_TROLLEY_CH, SHORT_FID_LN);
+MAKE_NMR_STRING(run_fixed, RUN_FIXED_CH, NMR_FID_LN);
+MAKE_NMR_STRING(run_fixed_st, RUN_FIXED_CH, SHORT_FID_LN);
+
+// flexible struct built from the basic nmr attributes.
 struct nmr_data {
-  std::vector<ULong64_t> clock_sec;
-  std::vector<ULong64_t> clock_usec;
-  std::vector<Double_t> fid_time;
+  std::vector<Double_t> sys_clock;
+  std::vector<Double_t> gps_clock;
+  std::vector<Double_t> dev_clock;
   std::vector<Double_t> snr;
+  std::vector<Double_t> len;
   std::vector<Double_t> freq;
-  std::vector<Double_t> freq_err;
+  std::vector<Double_t> ferr;
+  std::vector<Double_t> freq_zc;
+  std::vector<Double_t> ferr_zc;
+  std::vector<UShort_t> health;
+  std::vector<UShort_t> method;
   std::vector< std::array<UShort_t, NMR_FID_LN> > trace;
 
   inline void Resize(int size) {
-    clock_sec.resize(size);
-    clock_usec.resize(size);
-    fid_time.resize(size);
+    sys_clock.resize(size);
+    gps_clock.resize(size);
+    dev_clock.resize(size);
     snr.resize(size);
+    len.resize(size);
     freq.resize(size);
-    freq_err.resize(size);
+    ferr.resize(size);
+    freq_zc.resize(size);
+    ferr_zc.resize(size);
+    health.resize(size);
+    method.resize(size);
     trace.resize(size);
   }
 };
