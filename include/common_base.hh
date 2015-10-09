2f5e29e19242706ccbdebd3a102ced3405ad893d
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-08 23:02:43 -0500

    Revised logging, added dump type for vme like stuff.

--- include/common_base.hh
+++ include/common_base.hh
@@ -38,8 +38,7 @@ class CommonBase {
 
  protected:
   
-  static const int name_width_ = 16;
-  static const int time_width_ = 20;
+  static const int name_width_ = 12;
 
   // These should be defined in common_extdef.hh
   static int logging_verbosity_; 
@@ -52,6 +51,54 @@ class CommonBase {
   time_t timer_;
 
   // Printf style logging function (for max verbosity).
+  inline int LogDump(const char *format, ...) {
+    
+    if (logging_verbosity_ > 3) {
+      log_mutex_.lock(); 
+
+      while (!logstream_.is_open()) {
+        logstream_.open(logfile_, std::fstream::app | std::fstream::out);
+        usleep(10);
+      }
+
+      prepend(level::DUMP);      
+
+      va_list args;
+      va_start(args, format);
+      vsprintf(logstr_, format, args);
+      va_end(args);
+      
+      logstream_ << logstr_ << std::endl;
+      logstream_.close();
+      log_mutex_.unlock();      
+    }
+
+    return 0;
+  };
+  
+  // Prints a simple string to the log file (for max verbosity).
+  inline int LogDump(const std::string& message) {
+    
+    if (logging_verbosity_ > 3) {
+      log_mutex_.lock(); 
+
+      while (!logstream_.is_open()) {
+        logstream_.open(logfile_, std::fstream::app | std::fstream::out);
+        usleep(10);
+      }
+
+      prepend(level::DUMP);      
+
+      logstream_ << message << std::endl;
+      
+      logstream_.close();
+      log_mutex_.unlock();      
+    }
+
+    return 0;
+  };
+  
+  // Printf style logging function (for debug verbosity).
   inline int LogDebug(const char *format, ...) {
     
     if (logging_verbosity_ > 2) {
@@ -77,7 +124,7 @@ class CommonBase {
     return 0;
   };
   
-  // Prints a simple string to the log file (for max verbosity).
+  // Prints a simple string to the log file (for debug verbosity).
   inline int LogDebug(const std::string& message) {
     
     if (logging_verbosity_ > 2) {
@@ -85,16 +132,11 @@ class CommonBase {
 
       while (!logstream_.is_open()) {
         logstream_.open(logfile_, std::fstream::app | std::fstream::out);
+        usleep(10);
       }
 
-      time(&timer_);
-      auto t = localtime(&timer_);
+      prepend(level::DEBUG);      
 
-      char *tm_str = asctime(t);
-      tm_str[strlen(tm_str) - 1] = '\0';
-
-      logstream_ << std::left << std::setw(name_width_) << name_;
-      logstream_ << " [" << tm_str << "]:   *DEBUG*   ";
       logstream_ << message << std::endl;
       
       logstream_.close();
@@ -112,22 +154,16 @@ class CommonBase {
 
       while (!logstream_.is_open()) {
         logstream_.open(logfile_, std::fstream::app | std::fstream::out);
+        usleep(10);
       }
 
-      time(&timer_);
-      auto t = localtime(&timer_);
+      prepend(level::MESSAGE);      
 
-      char *tm_str = asctime(t);
-      tm_str[strlen(tm_str) - 1] = '\0';
-
-      logstream_ << std::left << std::setw(name_width_) << name_;
-      logstream_ << " [" << tm_str << "]:   MESSAGE   ";
-      
       va_list args;
       va_start(args, format);
       vsprintf(logstr_, format, args);
       va_end(args);
-      
+
       logstream_ << logstr_ << std::endl;
       logstream_.close();
       log_mutex_.unlock();      
@@ -144,16 +180,11 @@ class CommonBase {
 
       while (!logstream_.is_open()) {
         logstream_.open(logfile_, std::fstream::app | std::fstream::out);
+        usleep(10);
       }
 
-      time(&timer_);
-      auto t = localtime(&timer_);
+      prepend(level::MESSAGE);      
 
-      char *tm_str = asctime(t);
-      tm_str[strlen(tm_str) - 1] = '\0';
-
-      logstream_ << std::left << std::setw(name_width_) << name_;
-      logstream_ << " [" << tm_str << "]:   MESSAGE   ";
       logstream_ << message << std::endl;
       
       logstream_.close();
@@ -171,16 +202,10 @@ class CommonBase {
 
       while (!logstream_.is_open()) {
         logstream_.open(logfile_, std::fstream::app | std::fstream::out);
+        usleep(10);
       }
 
-      time(&timer_);
-      auto t = localtime(&timer_);
-
-      char *tm_str = asctime(t);
-      tm_str[strlen(tm_str) - 1] = '\0';
-
-      logstream_ << std::left << std::setw(name_width_) << name_;
-      logstream_ << " [" << tm_str << "]: **WARNING** ";
+      prepend(level::WARNING);      
       
       va_list args;
       va_start(args, format);
@@ -204,16 +229,11 @@ class CommonBase {
 
       while (!logstream_.is_open()) {
         logstream_.open(logfile_, std::fstream::app | std::fstream::out);
+        usleep(10);
       }
 
-      time(&timer_);
-      auto t = localtime(&timer_);
-
-      char *tm_str = asctime(t);
-      tm_str[strlen(tm_str) - 1] = '\0';
-
-      logstream_ << std::left << std::setw(name_width_) << name_;
-      logstream_ << " [" << tm_str << "]: **WARNING** ";
+      prepend(level::WARNING);      
+      
       logstream_ << warning << std::endl;
       
       logstream_.close();
@@ -230,16 +250,10 @@ class CommonBase {
 
     while (!logstream_.is_open()) {
       logstream_.open(logfile_, std::fstream::app | std::fstream::out);
+        usleep(10);
     }
     
-    time(&timer_);
-    auto t = localtime(&timer_);
-    
-    char *tm_str = asctime(t);
-    tm_str[strlen(tm_str) - 1] = '\0';
-    
-    logstream_ << std::left << std::setw(name_width_) << name_;
-    logstream_ << " [" << tm_str << "]: ***ERROR*** ";
+    prepend(level::ERROR);      
     
     va_list args;
     va_start(args, format);
@@ -261,16 +275,11 @@ class CommonBase {
 
     while (!logstream_.is_open()) {
       logstream_.open(logfile_, std::fstream::app | std::fstream::out);
+        usleep(10);
     }
     
-    time(&timer_);
-    auto t = localtime(&timer_);
-    
-    char *tm_str = asctime(t);
-    tm_str[strlen(tm_str) - 1] = '\0';
-    
-    logstream_ << std::left << std::setw(name_width_) << name_;
-    logstream_ << " [" << tm_str << "]: ***ERROR*** ";
+    prepend(level::ERROR);      
+
     logstream_ << error << std::endl;
     
     logstream_.close();
@@ -293,33 +302,33 @@ class CommonBase {
   inline void prepend(level lvl) {
 
     static timespec t;
-    static tm_start;
-    static lvl_msg;
+    static char tm_start[100];
+    static std::string lvl_msg;
 
     clock_gettime(CLOCK_REALTIME, &t);
-    tm_start = std::put_time(localtime(&t.tv_sec), "%F %T.");
+    std::strftime(tm_start, sizeof(tm_start), "[%F %T.", localtime(&t.tv_sec));
 
     switch (lvl) {
       case DUMP:
-        lvl_msg = std::string(" ==RAW== ");
+        lvl_msg = std::string("]  ==DUMP== ");
         break;
       case DEBUG:
-        lvl_msg = std::string(" =DEBUG= ");
+        lvl_msg = std::string("]  =DEBUG=  ");
         break;
       case MESSAGE:
-        lvl_msg = std::string(" MESSAGE ");
+        lvl_msg = std::string("]  MESSAGE  ");
         break;
       case WARNING:
-        lvl_msg = std::string("*WARNING*");
+        lvl_msg = std::string("] *WARNING* ");
         break;
       case ERROR:
-        lvl_msg = std::string("**ERROR**");
+        lvl_msg = std::string("] **ERROR** ");
         break;
     }
 
-    logstream_ << std::left << std::setw(time_width_) << tm_start;
-    logstream_ << std::setfill('0') << std::setw(6) << t.tv_nsec / 1000; 
-    logstream_ << lvl_msg;
+    logstream_ << tm_start << std::setfill('0') << std::setw(6) << t.tv_nsec / 1000;
+    logstream_ << lvl_msg << std::setfill(' ') << "{ " << std::left;
+    logstream_ << std::setw(name_width_) << name_ << " } : ";
   }
 };
 
